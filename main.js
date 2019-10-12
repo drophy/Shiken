630128cd9a4dacc514752ab18545b0a5523ce70f
drophy <44790265+drophy@users.noreply.github.com>
2019-10-12 02:10:59 -0500

    add navbar and code input

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+/// ANIMATE LOGO ///
 const logo = document.querySelectorAll("#logo>path");
 
-for(let i = 0; i < logo.length; i++)
-{
-   console.log(`Letter ${i} has length ${logo[i].getTotalLength()}`)
+for(let i = 0; i < logo.length; i++) {
    logo[i].addEventListener("animationend", (event) => event.path[0].classList.add("full-path"));
 }
