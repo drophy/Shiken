92cdc1881c65405d3aca7f37619b03fc0b1a9fcf
Ricardo Partida <remolinojiji@hotmail.com>
2019-11-26 11:24:48 -0600

    Adding items (unfinished)

--- voter.js
+++ voter.js
@@ -1,3 +1,11 @@
+class SleepingChick{
+   constructor(im){
+       this.clicks = 0;
+       this.anim = 1;
+       this.image = im;
+   }
+}
+
 console.log('voter.js connected');
 
 /// DATA BASE ///
@@ -22,6 +30,13 @@ let answerQuantity = 2;
 let correctAnswer = 'A';
 let totalTime = 10;
 
+let container = document.getElementById("ItemDiv");
+let activeItems = [false, false, false, false];
+let Hen = document.getElementById("ItemHen");
+let HenPos = -window.innerHeight -5;
+let chicks = [];
+let lastChick = 0;
+
 /// COUNT TIME ///
 let timePassed = 0; // time in seconds
 let playerAnswerTime;
@@ -32,7 +47,7 @@ for(let i = 0; i < totalTime-1; i++) {
    }, 1000*i);
 }
 
-setTimeout(finish, 1000*(totalTime-1));
+//setTimeout(finish, 1000*(totalTime-1));
 
 /// ENABLE / DISABLE ANSWERS ///
 let points;
@@ -55,6 +70,116 @@ function pickAnswer(event) {
    points = event.target.innerText == correctAnswer? 100 - 50*(timePassed/totalTime) : 0;
    // console.log(points); // DBUG
 }
+
+//DUMMY
+function itemDebug(){
+
+}
+//
+
+function initItems(){
+   if(activeItems[0] == true){
+      initHen();
+   }
+   if(activeItems[1] == true){
+      initChicks();
+   }
+}
+
+function initHen(){
+   Hen.style.left = -10 +"px";
+   Hen.style.display = "block";
+   var HenAnim = setInterval(HenFrame, 10);
+   Hen.addEventListener("click", function(){
+      HenPos -= 50;
+  });
+}
+
+function HenFrame(){    
+   if(HenPos >= 0){
+      if(HenPos > 0){
+         HenPos = 0;
+         Hen.style.top = HenPos +"px";
+      }
+         Hen.style.transform = `rotate(0)`;
+   }else{
+      HenPos+=1;
+      Hen.style.top = HenPos +"px";
+      Hen.style.transform = `rotate(${Math.cos(HenPos)}deg)`;    
+   }
+   //console.log("increment:" +increment);
+   //console.log("pos: " +HenDiv.style.left +" " +HenDiv.style.top);
+}
+
+function initChicks(){
+   var ChickID = [];
+   for(let i=0; i<3; i++){
+      //Add to HTML
+      container.innerHTML += `
+       <img src="https://github.com/drophy/Shiken/blob/master/images/Items/SleepingChick.png?raw=true" id="Chick${i}" class="SleepingChick">
+       `;
+
+      //Add to array
+      chicks.push(new SleepingChick(document.getElementById(`Chick${i}`)));
+      
+      //Rand position
+      chicks[i].image.style.top = Math.floor((Math.random() * (window.innerHeight -261) ) ) +"px";
+      chicks[i].image.style.left = Math.floor((Math.random() * (window.innerWidth -224) ) ) +"px";
+   
+      //ChickID[i] = setInterval(ChickFrame, 10);
+   
+   }
+
+   for(let i=0; i<chicks.length; i++){
+      document.getElementById(`Chick${i}`).addEventListener("click", function(){
+         lastChick = i;
+         if(chicks[i].clicks < 15){
+            chicks[i].clicks++;
+            console.log(chicks[i].clicks);
+         }else{
+            wakeUpChick(i);
+         }
+      });
+   }
+}
+
+function ChickFrame(){    
+   if(chicks[i].clicks < 15){
+      if(chicks[i].anim = 20){
+         chicks[i]
+      }
+   }   
+}
+
+function wakeUpChick(i){
+   chicks[i] = undefined;
+   document.querySelector(`#Chick${i}`).remove();
+}
+
+function initEgg(){
+   container.innerHTML += `
+   <img src="https://github.com/drophy/Shiken/blob/master/images/Items/Egg.png?raw=true" id="egg">
+   `;
+   let egg = document.getElementById("egg");
+   egg.style.top = 0 +"px";
+   egg.style.left = (window.innerWidth/4) +"px";
+   EggAnim = setInterval(EggFrame, 100);
+}
+
+let eggTop = 0;
+function EggFrame(){
+   let egg = document.getElementById("egg");
+   if(eggTop <= window.innerHeight){
+      if(eggTop > 30) eggTop++;
+      if(eggTop > 60) eggTop++;
+      eggTop++;
+      egg.style.top = eggTop +"px";
+   }else{
+      document.querySelector("#egg").remove;
+      clearInterval(EggFrame);
+   }
+}
+
 
 function initAnswers(answerQuantity) {
    document.querySelectorAll('.answer').forEach((answer, i) => {
